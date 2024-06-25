a2224b56555b527aa68993021775a1f4c0ba1a29
abhishekbhakat <abhishek.bhakat@hotmail.com>
2024-06-25 13:18:30 +0000

    Refactor

--- dags/example_hibernation_astro.py
+++ dags/example_hibernation_astro.py
@@ -10,61 +10,23 @@ import os
 
 DEPLOYMENT_ID = conf.get("astronomer", "casbin_deployment")
 ORGANIZATION_ID = urlparse(conf.get("webserver", "base_url")).netloc.split(".")[0]
-
-
-def hibernate_deployments():
-    """
-    Hibernates the deployment
-    """
-    api_token = os.getenv("ASTRO_API_TOKEN")
-    url = f"https://api.astronomer.io/platform/v1beta1/organizations/{ORGANIZATION_ID}/deployments/{DEPLOYMENT_ID}/hibernation-override"
-    response = request(
-        url=url,
-        method="POST",
-        headers={
-            "Authorization": f"Bearer {api_token}",
-            "Content-Type": "application/json"
-        },
-        json={
-            "isHibernating": True,
-        },
-        timeout=300,
-    )
-    print("Hibernating deployment...")
-    print(response.status_code)
-    print(response.json())
-
-def get_deployment_mode():
-    """
-    Get the deployment mode
-    """
-    api_token = os.getenv("API_TOKEN")
-    url = f"https://api.astronomer.io/platform/v1beta1/organizations/{ORGANIZATION_ID}/deployments/{DEPLOYMENT_ID}"
-    response = request(
-        url=url,
-        method="GET",
-        headers={
-            "Authorization": f"Bearer {api_token}",
-            "Content-Type": "application/json"
-        },
-        timeout=300,
-    )
-    return response.json().get("isDevelopmentMode")
+ASTRO_API_TOKEN = os.getenv("API_TOKEN")
 
 default_args = {
-    'owner': 'Astro',
-    'retries': 0,
-    'retry_delay': timedelta(minutes=5),
-    'start_date': datetime(2024, 1, 1)
+    "owner": "Avi",
+    "retries": 0,
+    "retry_delay": timedelta(minutes=5),
+    "start_date": datetime(2024, 1, 1),
 }
 
+
 @dag(
-        "hibernation_dag",
-        default_args=default_args,
-        schedule_interval='*/15 * * * *',
-        catchup=False, 
-        max_active_runs=1,
-        doc_md="""
+    "Auto_Hibernation_Dag",
+    default_args=default_args,
+    schedule_interval="*/15 * * * *",
+    catchup=False,
+    max_active_runs=1,
+    doc_md="""
 DAG to hibernate deployment based on the threshold interval and deployment mode.
 Requires the following environment variables to be set:
 - API_TOKEN: API Token
@@ -73,24 +35,35 @@ Requires the following environment variables to be set:
 This DAG will check if any DAGs ran in previous THRESHOLD_TIME_IN_MINS minutes. 
 If no DAGs ran, it will hibernate the deployment, else will poll every 15 minutes.
 """,
-        tags=['example']
-        )
+    tags=["example"],
+)
 def example_hibernation_dag():
     @task.branch(task_display_name="Check Deployment Mode 🧑🏻‍💻")
     def check_deployment_type(**context):
-        is_development = get_deployment_mode()
+        url = f"https://api.astronomer.io/platform/v1beta1/organizations/{ORGANIZATION_ID}/deployments/{DEPLOYMENT_ID}"
+        response = request(
+            url=url,
+            method="GET",
+            headers={
+                "Authorization": f"Bearer {ASTRO_API_TOKEN}",
+                "Content-Type": "application/json",
+            },
+            timeout=300,
+        )
+        is_development = response.json().get("isDevelopmentMode")
         print(f"Development_mode: {is_development}")
         if is_development:
             return "check_hibernation_condition"
         else:
             return "end"
-       
+
     @task.branch(task_display_name="Check Hibernation Condition ❄")
     def check_hibernation_condition():
         threshold_time_in_mins = int(os.getenv("THRESHOLD_TIME_IN_MINS", "15"))
 
         dag_runs = DagRun.find(
-            execution_start_date=datetime.now(timezone.utc) - timedelta(minutes=int(threshold_time_in_mins)),
+            execution_start_date=datetime.now(timezone.utc)
+            - timedelta(minutes=int(threshold_time_in_mins)),
         )
         dag_runs = [dag_run for dag_run in dag_runs if dag_run.dag_id != "hibernation_dag"]
         print(f"DAGs ran below threshold limit {threshold_time_in_mins}: {len(dag_runs)}")
@@ -98,19 +71,31 @@ def example_hibernation_dag():
             return "hibernate_deployment"
         else:
             return "end"
-       
+
     @task(task_id="hibernate_deployment", task_display_name="Hibernate Deployment 🛌")
     def hibernate():
-        hibernate_deployments()
+        url = f"https://api.astronomer.io/platform/v1beta1/organizations/{ORGANIZATION_ID}/deployments/{DEPLOYMENT_ID}/hibernation-override"
+        response = request(
+            url=url,
+            method="POST",
+            headers={
+                "Authorization": f"Bearer {ASTRO_API_TOKEN}",
+                "Content-Type": "application/json",
+            },
+            json={
+                "isHibernating": True,
+            },
+            timeout=300,
+        )
+        print(response.status_code)
+        print(response.json())
+        return response.json()
 
-    end = EmptyOperator(task_id="end", trigger_rule="none_failed", task_display_name="End")
+    end = EmptyOperator(
+        task_id="end", trigger_rule="none_failed", task_display_name="End 🏁"
+    )
 
     check_deployment_type() >> check_hibernation_condition() >> hibernate() >> end
 
-example_hibernation_dag()
 
-
-
-
-
-
+dag_obj = example_hibernation_dag()
