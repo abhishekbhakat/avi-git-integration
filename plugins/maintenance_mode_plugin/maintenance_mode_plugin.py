4d154314c54dd51d0bc795344996ae2b9252ef24
abhishekbhakat <abhishek.bhakat@hotmail.com>
2024-07-01 11:30:52 +0000

    fix maintenance mode

--- plugins/maintenance_mode_plugin/maintenance_mode_plugin.py
+++ plugins/maintenance_mode_plugin/maintenance_mode_plugin.py
@@ -1,35 +1,36 @@
 from airflow.plugins_manager import AirflowPlugin
-from flask import Blueprint, request, render_template
 from airflow.www.app import csrf
-from datetime import datetime
+from flask_appbuilder import expose, BaseView as AppBuilderBaseView
+from flask import Blueprint
+import os
+
+# Get the directory of the current file
+current_dir = os.path.dirname(os.path.abspath(__file__))
+template_folder = os.path.join(current_dir, "templates")
 
 bp = Blueprint(
     "maintenance_mode",
     __name__,
-    template_folder="templates",
+    template_folder=template_folder,
     static_folder="static",
     static_url_path="/static/maintenance_mode",
 )
 
-@bp.route("/maintenance", methods=["GET", "POST"])
-@csrf.exempt
-def maintenance_mode():
-    if request.method == "POST":
-        start_time = request.form.get("start_time")
-        end_time = request.form.get("end_time")
-        task_handling = request.form.get("task_handling")
-        # Process the form data here
-    
-    return render_template("maintenance_mode_form.html")
+class MaintenanceModeView(AppBuilderBaseView):
+    default_view = "maintenance"
+    template_folder = template_folder
+
+    @expose("/")
+    @csrf.exempt
+    def maintenance(self):
+        return self.render_template("maintenance_mode_form.html")
+
+v_appbuilder_view = MaintenanceModeView()
+v_appbuilder_package = {"name": "Maintenance Mode",
+                        "category": "Admin",
+                        "view": v_appbuilder_view}
 
 class MaintenanceModePlugin(AirflowPlugin):
     name = "maintenance_mode_plugin"
     flask_blueprints = [bp]
-    appbuilder_menu_items = [
-        {
-            "name": "Maintenance Mode",
-            "category": "Admin",
-            "category_icon": "fa-wrench",
-            "href": "/maintenance",
-        }
-    ]
+    appbuilder_views = [v_appbuilder_package]
