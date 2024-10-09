69e389ba188616b2a130bca38f5475db05b6b2fb
abhishekbhakat <abhishek.bhakat@hotmail.com>
2024-10-09 11:05:30 +0000

    Forced RBAC

---
+++ plugins/rbac_plugin/__init__.py
@@ -0,0 +1,3 @@
+from rbac_plugin.policies import task_instance_mutation_hook
+
+__all__ = ['task_instance_mutation_hook']
---
+++ plugins/rbac_plugin/policies/__init__.py
@@ -0,0 +1,37 @@
+from typing import TYPE_CHECKING
+
+
+from airflow.policies import hookimpl
+from airflow.models import Log
+from airflow import settings
+from airflow.models.taskinstance import TaskInstance
+
+@hookimpl
+def task_instance_mutation_hook(task_instance: TaskInstance):
+    def get_user():
+        session = settings.Session()
+        dag_log_entry = session.query(Log).filter(
+            Log.dag_id == task_instance.dag_id,
+            Log.event.in_(['trigger'])
+        ).order_by(Log.dttm.desc()).first()
+        
+        task_log_entry = session.query(Log).filter(
+            Log.dag_id == task_instance.dag_id,
+            Log.task_id == task_instance.task_id,
+            Log.event.in_(['clear', 'retry', 'cli_task_run'])
+        ).order_by(Log.dttm.desc()).first()
+        
+        if task_log_entry and (not dag_log_entry or task_log_entry.dttm > dag_log_entry.dttm):
+            return task_log_entry.owner if task_log_entry.owner else None
+        elif dag_log_entry and dag_log_entry.owner:
+            return dag_log_entry.owner if dag_log_entry.owner else None
+        else:
+            return None
+        
+
+    user = get_user()
+    user_list = ['user1', 'user2']
+    # instead of user_list make Astro API call to fetch users allowed to run the task by custom roles or which way suitable.
+    if user not in user_list:
+        print(f"User {user} is not allowed to run this task")
+        task_instance.state = 'skipped'
