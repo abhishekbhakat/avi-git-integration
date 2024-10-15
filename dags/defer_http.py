dfcbe11821d095e3caa3cd296d579d7125450b46
abhishekbhakat <abhishek.bhakat@hotmail.com>
2024-10-15 10:49:38 +0000

    add sensor

--- dags/defer_http.py
+++ dags/defer_http.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.providers.http.operators.http import HttpOperator
+from airflow.providers.http.sensors.http import HttpSensor
 from datetime import datetime, timedelta
 
 default_args = {
@@ -22,6 +23,14 @@ with DAG(
         headers={"Content-Type": "application/json"},
         log_response=True,
         deferrable=True,  # Enable deferrable
+        response_check=lambda response: response.status_code == 200,
+    )
+
+    fetch_sensor = HttpSensor(
+        task_id='fetch_sensor',
+        http_conn_id='my_http_conn',
+        endpoint='api/v1/data',
+        poke_interval=5,
         response_check=lambda response: response.status_code == 200,
     )
 
